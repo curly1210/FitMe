5ef7b73666ee0624246e538f8d794db71580322c
bazou-bou <giaabaoo0510@gmail.com>
2025-05-24 22:12:44 +0700

    be/feture: Api danh sách, thêm, sửa của quản lí biến thể

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\UserController;
+use App\Http\Controllers\Api\VariationController;
 
 Route::get('/users', [UserController::class, 'index']);
 // Route Authen
@@ -68,7 +69,20 @@ Route::get('/users', [UserController::class, 'index']);
 
 
 
+Route::prefix('variations')->group(function () {
+    // list màu sắc và kích thước
+    Route::get('/', [VariationController::class, 'index']);
 
+    // màu sắc
+    Route::post('/color', [VariationController::class, 'storeColor']);
+    Route::post('/color/{id}', [VariationController::class, 'updateColor']);
+
+
+    // kích th`ước
+    Route::post('/size', [VariationController::class, 'storeSize']);
+    Route::post('/size/{id}', [VariationController::class, 'updateSize']);
+
+});
 
 
 
