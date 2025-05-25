465ff519526481d63cbb944a12e678f5a535f40f
bazou-bou <giaabaoo0510@gmail.com>
2025-05-25 22:52:43 +0700

    be/fix: api resource

--- BE/app/Http/Resources/SizeResource.php
+++ BE/app/Http/Resources/SizeResource.php
@@ -11,9 +11,9 @@ class SizeResource extends JsonResource
         return [
             'id'        => $this->id,
             'name'      => $this->name,
-            'is_active' => $this->is_active,
-            'created_at'=> $this->created_at,
-            'updated_at'=> $this->updated_at,
+            // 'is_active' => $this->is_active,
+            // 'created_at'=> $this->created_at,
+            // 'updated_at'=> $this->updated_at,
         ];
     }
 }
