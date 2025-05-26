540219f38325c053e6186f9e26d141cf2f2f5088
NGB16-Coder <giabao16032005@gmail.com>
2025-05-26 11:50:00 +0700

    be/feature/thêm logout, refresh

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 // Route Authen
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/refresh', [AuthController::class, 'refresh']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
